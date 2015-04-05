b8ff00140cb5b3dd7f930605431a871acfc8f3db
Salim Virani <smile@saintsal.com>
2015-04-05 15:53:23 +0300

    sorta works

---
+++ app.js
@@ -0,0 +1,72 @@
+(function() {
+var app = angular.module('signally', []);
+
+app.filter('reverse', function() {
+    return function(items) {
+        return items.slice().reverse();
+    };
+});
+
+
+app.controller('AppController', function() {
+    this.signals = signals;
+});
+
+app.controller('SignalController', function() {
+    this.signal = {};
+    this.addSignal = function(signal) {
+        signals.push(this.signal);
+        this.signal = [];
+    };
+});
+
+var signals = [{
+    phrase: 'stuff'
+    }, {
+    phrase: 'more stuff'
+    }
+];
+
+app.directive('contenteditable', ['$sce', function($sce) {
+    return {
+      restrict: 'A', // only activate on element attribute
+      require: '?ngModel', // get a hold of NgModelController
+      link: function(scope, element, attrs, ngModel) {
+        if (!ngModel) return; // do nothing if no ng-model
+
+        // Specify how UI should be updated
+        ngModel.$render = function() {
+          element.html($sce.getTrustedHtml(ngModel.$viewValue || ''));
+        };
+
+        // Listen for change events to enable binding
+        element.on('blur keyup change', function() {
+          // If strip-br attribute is provided then we strip this out
+           ( attrs.stripBr && html == '<br>' ) {
+          element.html($sce.getTrustedHtml(ngModel.$viewValue || ''));
+
+          }
+          scope.$evalAsync(read);
+        });
+        element.on('keydown',function (event) {
+            if (event.which == 13) {
+                $("#phraseform").find('[type=submit]').trigger('click');
+            }
+        });
+
+        read(); // initialize
+
+        // Write data to the model
+        function read() {
+          var html = element.html();
+          // When we clear the content editable the browser leaves a <br> behind
+          // If strip-br attribute is provided then we strip this out
+          if ( attrs.stripBr && html == '<br>' ) {
+            html = '';
+          }
+          ngModel.$setViewValue(html);
+        }
+      }
+    };
+  }]);
+})();
